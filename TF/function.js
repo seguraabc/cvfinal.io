68556411652928b54c2d86d82fc912c8ebd9dc16
seguraabc <122451838+seguraabc@users.noreply.github.com>
2023-01-27 20:43:49 -0300

    Add files via upload

---
+++ TF/function.js
@@ -0,0 +1,45 @@
+
+
+
+document.getElementById("boton1").addEventListener("click", function () {
+    document.getElementById('empresa1').style.display ='block';
+    document.getElementById('boton1').style.display= 'none';
+    document.getElementById('boton1b').style.display= 'block';
+     
+})
+
+document.getElementById("boton1b").addEventListener("click", function () {
+    document.getElementById('empresa1').style.display ='none';
+    document.getElementById('boton1').style.display= 'block';
+    document.getElementById('boton1b').style.display= 'none';
+    
+})
+
+document.getElementById("boton2").addEventListener("click", function () {
+    document.getElementById('empresa2').style.display ='block';
+    document.getElementById('boton2').style.display= 'none';
+    document.getElementById('boton2b').style.display= 'block';
+    
+})
+
+document.getElementById("boton2b").addEventListener("click", function () {
+    document.getElementById('empresa2').style.display ='none';
+    document.getElementById('boton2').style.display= 'block';
+    document.getElementById('boton2b').style.display= 'none';
+    // document.getElementById('boton3').innerHTML ="-info";
+    
+})
+
+document.getElementById("boton3").addEventListener("click", function () {
+    document.getElementById('empresa3').style.display ='block';
+    document.getElementById('boton3').style.display= 'none';
+    document.getElementById('boton3b').style.display= 'block';
+   
+})
+
+document.getElementById("boton3b").addEventListener("click", function () {
+    document.getElementById('empresa3').style.display ='none';
+    document.getElementById('boton3').style.display= 'block';
+    document.getElementById('boton3b').style.display= 'none';
+    
+})
